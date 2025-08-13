528297f03ea399ad5ebf5fc679d4e81fc2e3f26e
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2025-08-13 19:26:12 +0600

    Changes in about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
           <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
             <span className="text-white">I am a </span>
             <ReactTypingEffect
-              text={["Fullstack Developer", "Frontend Developer", "Coder"]}
+              text={["Frontend Developer", "Fullstack Developer", "Coder"]}
               speed={100}
               eraseSpeed={50}
               typingDelay={500}
